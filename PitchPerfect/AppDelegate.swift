170dae5fa019dad2f3c91d7dbf7beef7239a0953
Dhairya Nishar <dhairyatnishar@gmail.com>
2020-03-13 11:58:28 -0700

    Update AppDelegate.swift

--- PitchPerfect/AppDelegate.swift
+++ PitchPerfect/AppDelegate.swift
@@ -41,5 +41,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         return true
     }
-
 }
